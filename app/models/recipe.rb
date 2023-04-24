a0a14169a0e41bf0ae82f3cdd34800c140c2b391
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-24 22:07:54 +0300

    Fix linter err

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
 
-  
+
   validates :name, presence: true
   validates :preparation_time, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
-    has_many :recipes, dependent: :destroy
-    
+  has_many :recipes, dependent: :destroy
+
   validates :name, presence: true, length: { minimum: 0, maximum: 50 }
 
   def list_recipes
     Recipe.where(user_id: id)
   end
-  
 end
