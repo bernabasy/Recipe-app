6ce3ffde0205d68e3ad3f9742b0feedaa93b3dc2
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-26 16:35:20 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  # resources :users
+  
+  #   resources :recipes, except: [:update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  resources :recipes
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
+  root "recipes#index"
 end
