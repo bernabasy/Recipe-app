4a9bfbf4111718e3915ec8a922a1b06a3f0bd5a7
michaelamponsah <amponsahmichaelkwame@gmail.com>
2023-04-26 15:47:21 +0000

    Modify controllers and views of Food resource to fit requirements
    Implemented actions to display data of logged-in user only and save foods data

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,8 +1,30 @@
 class FoodsController < ApplicationController
+  # before_action :set_recipe, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+
   def index
-    @all_foods = Food.get_all_foods
+    @all_foods = Food.get_all_foods(current_user.id)
   end
 
   def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+    @food.user_id = current_user.id
+
+    if @food.save
+      redirect_to foods_path, notice: "Food was successfully created."
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
   end
 end
