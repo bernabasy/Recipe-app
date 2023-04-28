6bc1de5435d57de6a19786400c2ea62afc2f9681
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-04-28 19:45:23 +0300

    Fix linters

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,7 +1,6 @@
 class PublicRecipesController < ApplicationController
   before_action :authenticate_user!
 
-
   def index
     @public_recipes = Recipe.includes(:recipe_foods, :user).where(public: true).order(created_at: :desc)
   end
